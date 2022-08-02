66dde99399f5ba4c4664433d0f04e0cc99e2101c
David Justice <davidjustice28@gmail.com>
2022-08-02 12:05:19 -0400

    update protocol to https from http

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function displayWeather() {
     }
     
     async function getWeatherData(City) {
-        let promise = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${City},${state},us&units=imperial&APPID=f5b2fcba915a037ebff7f36dc5806378`);
+        let promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${City},${state},us&units=imperial&APPID=f5b2fcba915a037ebff7f36dc5806378`);
         let weatherObject = await promise.json();
         console.log(weatherObject);
         return weatherObject ;
