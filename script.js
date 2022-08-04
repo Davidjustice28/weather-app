8ea4b4cbe3b9cace21cebe0a8dc56ab63c8e9d6c
David Justice <davidjustice28@gmail.com>
2022-08-04 12:45:32 -0400

    added changeablebackground images, temp is a integer, and dropdown button for more info

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ function displayWeather() {
     console.log(desiredCity);
     let state = document.getElementById('state-input').value;
 
+    const moreInfoDiv = document.getElementById('addition-info'); 
+
+    function displayMoreInfo() {
+        moreInfoDiv.style.display = 'flex';
+    }
+
     function loadModule() {
         const module = document.getElementById('module');
         module.style.display = 'flex';
@@ -21,18 +27,67 @@ function displayWeather() {
     getWeatherData(desiredCity).then((data) => {
         const cityDisplay = document.querySelector('h1');
         const tempDisplay = document.getElementById('temp-display');
-        const rangeDisplay = document.getElementById('temp-range')
+        const rangeDisplay = document.getElementById('temp-range');
         const conditionDisplay = document.getElementById('condition-display');
+        const feelsLikeTemp = document.getElementById('feels-like-temp');
+        const humidty = document.getElementById('humidity');
+        const windSpeed = document.getElementById('wind-speed');
+
+        switch (data.weather[0].main) {
+            case 'Clouds':
+                document.body.style.backgroundImage = 'url(./images/Cloudy.PNG)' 
+                break;
+            case 'Clear':
+                document.body.style.backgroundImage = 'url(./images/Clear.PNG)'
+                break;
+            case 'Rain':
+                document.body.style.backgroundImage = 'url(./images/Rain.PNG)'
+                break;
+            case 'Mist':
+                document.body.style.backgroundImage = 'url(./images/Fog.PNG)'
+                break;
+            case 'Fog':
+                document.body.style.backgroundImage = 'url(./images/Fog.PNG)'
+                break;
+                break;
+            default:
+                document.body.style.backgroundColor = 'blue'
+                break;
+        }
+
         cityDisplay.innerText = data.name;
-        tempDisplay.innerText = `${data.main.temp}°F`;
+        tempDisplay.innerText = Math.round(data.main.temp) +'°F';
         rangeDisplay.innerText =`Low ${data.main.temp_min}° | High ${data.main.temp_max}°`;
         conditionDisplay.innerText = data.weather[0].description;
-    }).then(() => {
+        feelsLikeTemp.innerText = `Feels Like - ${data.main.feels_like}°`;
+        if(data.main.humidity == undefined) {
+            humidty.innerText = 'Humidity - NA';
+        } else {
+            humidty.innerText = `Humidity - ${data.main.humidity}%`;
+        }
+        windSpeed.innerText = `Windspeed - ${data.wind.speed} mph`;})
+        .then(() => {
         document.getElementById('city-input').value = null;
         document.getElementById('state-input').value = null;
         loadModule();
     });
 }
 
+function displayMoreInfo() {
+    moreInfoDiv.style.display = 'flex';
+}
+
+function closeMoreInfo() {
+    moreInfoDiv.style.display = 'none';
+}
+
+const moreInfoDiv = document.getElementById('addition-info'); 
+
 const myButton = document.querySelector('button');
 myButton.addEventListener('click', displayWeather);
+
+const infoButton = document.getElementById('info-button');
+infoButton.addEventListener('click',displayMoreInfo);
+
+const icon = document.getElementById('icon');
+icon.addEventListener('click', closeMoreInfo);
